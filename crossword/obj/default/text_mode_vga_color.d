1aea1c040a6da6a06a462e7d61a97831fc90544f
Wesley Wu <wwu70@illinois.edu>
2022-05-07 15:46:25 -0500

    committed c code and bsp files to repo

---
+++ crossword/obj/default/text_mode_vga_color.d
@@ -0,0 +1,47 @@
+obj/default/text_mode_vga_color.o: text_mode_vga_color.c \
+ ../crossword_bsp/system.h ../crossword_bsp/linker.h \
+ ../crossword_bsp//HAL/inc/alt_types.h text_mode_vga_color.h \
+ ../crossword_bsp//drivers/inc/altera_avalon_spi.h \
+ ../crossword_bsp//drivers/inc/altera_avalon_spi_regs.h \
+ ../crossword_bsp//HAL/inc/io.h ../crossword_bsp//HAL/inc/alt_types.h \
+ ../crossword_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../crossword_bsp//HAL/inc/sys/alt_irq.h \
+ ../crossword_bsp//HAL/inc/nios2.h usb_kb/GenericMacros.h \
+ usb_kb/GenericTypeDefs.h usb_kb/HID.h usb_kb/MAX3421E.h \
+ usb_kb/transfer.h usb_kb/usb_ch9.h usb_kb/USB.h
+
+../crossword_bsp/system.h:
+
+../crossword_bsp/linker.h:
+
+../crossword_bsp//HAL/inc/alt_types.h:
+
+text_mode_vga_color.h:
+
+../crossword_bsp//drivers/inc/altera_avalon_spi.h:
+
+../crossword_bsp//drivers/inc/altera_avalon_spi_regs.h:
+
+../crossword_bsp//HAL/inc/io.h:
+
+../crossword_bsp//HAL/inc/alt_types.h:
+
+../crossword_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../crossword_bsp//HAL/inc/sys/alt_irq.h:
+
+../crossword_bsp//HAL/inc/nios2.h:
+
+usb_kb/GenericMacros.h:
+
+usb_kb/GenericTypeDefs.h:
+
+usb_kb/HID.h:
+
+usb_kb/MAX3421E.h:
+
+usb_kb/transfer.h:
+
+usb_kb/usb_ch9.h:
+
+usb_kb/USB.h:
---
+++ crossword/obj/default/usb_kb/MAX3421E.d
@@ -0,0 +1,48 @@
+obj/default/usb_kb/MAX3421E.o: usb_kb/MAX3421E.c \
+ ../crossword_bsp/system.h ../crossword_bsp/linker.h \
+ usb_kb/../usb_kb/project_config.h \
+ usb_kb/../usb_kb/../usb_kb/GenericMacros.h \
+ usb_kb/../usb_kb/../usb_kb/GenericTypeDefs.h \
+ usb_kb/../usb_kb/../usb_kb/HID.h usb_kb/../usb_kb/../usb_kb/MAX3421E.h \
+ usb_kb/../usb_kb/../usb_kb/transfer.h \
+ usb_kb/../usb_kb/../usb_kb/usb_ch9.h usb_kb/../usb_kb/../usb_kb/USB.h \
+ ../crossword_bsp//drivers/inc/altera_avalon_spi.h \
+ ../crossword_bsp//HAL/inc/alt_types.h \
+ ../crossword_bsp//drivers/inc/altera_avalon_spi_regs.h \
+ ../crossword_bsp//HAL/inc/io.h ../crossword_bsp//HAL/inc/alt_types.h \
+ ../crossword_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../crossword_bsp//HAL/inc/sys/alt_stdio.h
+
+../crossword_bsp/system.h:
+
+../crossword_bsp/linker.h:
+
+usb_kb/../usb_kb/project_config.h:
+
+usb_kb/../usb_kb/../usb_kb/GenericMacros.h:
+
+usb_kb/../usb_kb/../usb_kb/GenericTypeDefs.h:
+
+usb_kb/../usb_kb/../usb_kb/HID.h:
+
+usb_kb/../usb_kb/../usb_kb/MAX3421E.h:
+
+usb_kb/../usb_kb/../usb_kb/transfer.h:
+
+usb_kb/../usb_kb/../usb_kb/usb_ch9.h:
+
+usb_kb/../usb_kb/../usb_kb/USB.h:
+
+../crossword_bsp//drivers/inc/altera_avalon_spi.h:
+
+../crossword_bsp//HAL/inc/alt_types.h:
+
+../crossword_bsp//drivers/inc/altera_avalon_spi_regs.h:
+
+../crossword_bsp//HAL/inc/io.h:
+
+../crossword_bsp//HAL/inc/alt_types.h:
+
+../crossword_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../crossword_bsp//HAL/inc/sys/alt_stdio.h:
